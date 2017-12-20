01cf23f5d32253500e445cb92e12c3a652b9c6e2
Andrew Murray <darbyguitars@gmail.com>
2017-12-20 15:32:35 -0600

    more env

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,16 +1,19 @@
 //test helper does these things
-
-//set up testing enviornment to run like a browser in the command line
 import jsdom from 'jsdom'
 import _$ from 'jquery'
+import TestUtils from 'react-addons-test-utils'
+
+//set up testing enviornment to run like a browser in the command line
+
 
 global.document = jsdom.jsdom('<doctype html><html><body><body></html>')
 global.window = global.document.defaultView
 const $ = _$(global.window)
 
 //build render component to that should render a given react class
-
-
+function renderComponent(ComponentClass) {
+  // const componentInstance =
+}
 
 //build helper for simulating eventsource
 
