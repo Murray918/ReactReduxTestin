73c84ec49b90294e6829846fd150284a9928038c
Andrew Murray <darbyguitars@gmail.com>
2017-12-20 18:10:58 -0600

    here we are still setting up the test helper but now we have implemented a store and some props for our component to access

--- test/test_helper.js
+++ test/test_helper.js
@@ -2,6 +2,12 @@
 import jsdom from 'jsdom'
 import _$ from 'jquery'
 import TestUtils from 'react-addons-test-utils'
+import ReactDom from 'react-dom'
+import { expect } from 'chai'
+import React from 'react'
+import { Provider } from 'react-redux'
+import {  createStore } from 'redux'
+import reducers from '../src/reducers'
 
 //set up testing enviornment to run like a browser in the command line
 
@@ -11,8 +17,17 @@ global.window = global.document.defaultView
 const $ = _$(global.window)
 
 //build render component to that should render a given react class
-function renderComponent(ComponentClass) {
-  // const componentInstance =
+
+//redner component will take three args #1 the component #2 any props that need to be placed on it #3 any application state that needs to be present
+function renderComponent(ComponentClass, props, state) {
+  const componentInstance = TestUtils.renderIntoDocument(
+    //the second arg to the create store is some initial state
+    <Provider store = {createStore(reducers, state)}>
+    <ComponentClass {...props} />
+  </Provider>
+  )
+
+  return $(ReactDom.findDOMNode(componentInstance))
 }
 
 //build helper for simulating eventsource
@@ -20,3 +35,4 @@ function renderComponent(ComponentClass) {
 
 
 //set up chai jquery
+export { renderComponent, expect }
